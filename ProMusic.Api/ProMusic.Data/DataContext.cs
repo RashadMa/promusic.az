ba18c672cb4b595a907ee1dcaccd36951fa5a371
Rashad <87971037+RashadM0@users.noreply.github.com>
2022-02-25 14:26:22 +0400

    First test commit

---
+++ ProMusic.Api/ProMusic.Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProMusic.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfiguration(new CategoryConfig());
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
